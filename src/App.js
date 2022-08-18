9d4cd963754515a32122a65f53dbefe1becc8736
Daniel <dselasea@gmail.com>
2022-08-18 19:44:30 +0000

    Completed little modal challenge with conditional rendering and function as props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Title from "./components/Title";
 import Modal from "./components/Modal";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
   const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     {title: "Mario's birthday bash", id: 1},
@@ -11,7 +12,7 @@ function App() {
     {title: "Race on the moo moo farm", id: 3},
   ]);
 
-  console.log(showEvents);
+  console.log(showModal);
 
   const handleClick = (id) => {
     setEvents((prevEvents) => {
@@ -21,12 +22,15 @@ function App() {
     })
   }
 
+  const handleClose = () => {
+    setShowModal(false);
+  }
+
   const subtitle = "All the lastest events in Marioland";
 
   return (
     <div className="App">
       <Title  title="Events in Your Area" subtitle={subtitle}/>
-      <Title title="Another title" subtitle="Another subtitle"/>
       {showEvents && (<div>
         <button onClick={() => setShowEvents(false)}>Hide events</button>
       </div>)}
@@ -45,11 +49,11 @@ function App() {
           <h2>10% Off Coupon Code!!!</h2>
           <p>Use the code NINJA10 at the checkout.</p>
       </Modal> */}
-      <Modal>
+      <button onClick={() => setShowModal(true)}>Show Terms & Conditions</button>
+      {showModal && <Modal handleClose={handleClose}>
         <h2>Terms and Conditions</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam repudiandae sequi doloremque rem corrupti accusantium? Ex incidunt nostrum neque sapiente maiores voluptatibus tempora debitis sint! Quod cumque temporibus veritatis minima ducimus error commodi laborum, sed saepe aut, quidem ipsa at.</p>
-        <a href="#">Find out more...</a>
-      </Modal>
+      </Modal>}
     </div>
   );
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import "./Modal.css";
 
-function Modal({children}) {
+function Modal({children, handleClose}) {
   return (
     <div className='modal-backdrop'>
         <div className='modal'>
             {children}
+            <button onClick={handleClose}>Close</button>
         </div>
     </div>
   )
