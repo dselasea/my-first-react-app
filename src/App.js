2eedbe25b30803827da441372970168337ef8609
Daniel <dselasea@gmail.com>
2022-08-18 20:29:19 +0000

    Breaking down components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, {useState} from 'react';
 import Title from "./components/Title";
 import Modal from "./components/Modal";
+import EventList from "./components/EventList";
 
 function App() {
   const [showModal, setShowModal] = useState(false);
@@ -38,12 +39,7 @@ function App() {
         <button onClick={() => setShowEvents(true)}>Show events</button>
       </div>}
       
-      {showEvents && events.map((event, index) => (
-        <React.Fragment key={event.id}>
-          <h2>{index + 1} - {event.title}</h2>
-          <button onClick={() => {handleClick(event.id)}}>Delete event</button>
-        </React.Fragment>
-      ))}
+      {showEvents && <EventList events={events} handleClick={handleClick}/>}
       
       {/* <Modal>
           <h2>10% Off Coupon Code!!!</h2>
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import "./Modal.css";
 
 function Modal({children, handleClose}) {
-  return (
+  return ReactDOM.createPortal((
     <div className='modal-backdrop'>
         <div className='modal'>
             {children}
             <button onClick={handleClose}>Close</button>
         </div>
     </div>
-  )
+  ), document.body)
 }
 
 export default Modal
