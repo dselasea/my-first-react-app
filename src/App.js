959c407077355995dadcff39089695bc766f9ee9
Daniel <dselasea@gmail.com>
2022-08-18 17:03:36 +0000

    previous Events state and useState

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-  const [name, setName] = useState("Mario");
-  const [events, setEVents] = useState([
+  const [events, setEvents] = useState([
     {title: "Mario's birthday bash", id: 1},
     {title: "Bowser's live stream", id: 2},
     {title: "Race on the moo moo farm", id: 3},
   ]);
 
-  const handleClick = () => {
-    setName("Luigi");
-    console.log(name);
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id;
+      })
+    })
   }
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
       {events.map((event, index) => (
         <div key={event.id}>
           <h2>{index + 1} - {event.title}</h2>
+          <button onClick={() => {handleClick(event.id)}}>Delete event</button>
         </div>
       ))}
     </div>
