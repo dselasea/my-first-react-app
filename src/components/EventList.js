33bdf7dec8508faa591460db776c1a69494cd330
Daniel <dselasea@gmail.com>
2022-08-19 11:49:33 +0000

    Css modules

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import styles from "./EventList.module.css"
 
 function EventList({events, handleClick}) {
   return (
     <div>
         {events.map((event, index) => (
-        <React.Fragment key={event.id}>
+        <div className={styles.card} key={event.id}>
           <h2>{index + 1} - {event.title}</h2>
           <button onClick={() => {handleClick(event.id)}}>Delete event</button>
-        </React.Fragment>
+        </div>
       ))}
     </div>
   )
