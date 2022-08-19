21555e6383485e9dfff48befaf82a9e31abe3a24
Daniel <dselasea@gmail.com>
2022-08-19 10:28:37 +0000

    Component styling

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
+import "./Title.css";
 
 function Title({title, subtitle}) {
   return (
-    <>
+    <div className="title-block">
         <h1 className='title'>{title}</h1>
         <br />
         <h2 className="subtitle">{subtitle}</h2>
-    </>
+    </div>
   )
 }
 
